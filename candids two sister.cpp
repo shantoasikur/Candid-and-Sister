9f7f0026d6e7177a3827ffc89274a37a3381abac
Md.Asikur Rahaman <141675776+shantoasikur@users.noreply.github.com>
2023-08-10 04:52:41 +0600

    Add files via upload

---
+++ candids two sister.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+            int n;
+            int count=0;
+
+            cin>>n;
+            if(n%2==0){
+                count=(n/2-1);
+                cout<<count<<endl;
+            }
+            else{
+                count=n/2;
+                cout<<count<<endl;
+            }
+
+
+
+
+
+            }
+            return 0;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
